4e5367922c3da73d11403b3ff0ded3660109dd5f
hermani <hermani457@gmail.com>
2024-04-17 13:58:48 -0400

    movingtext added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import MovingText from "./components/MovingText";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <MovingText />
     </>
   );
 }
---
+++ src/components/MovingText.jsx
@@ -0,0 +1,59 @@
+import { useRef, useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export default function Home() {
+  const firstText = useRef(null);
+  const secondText = useRef(null);
+  const slider = useRef(null);
+  let xPercent = 0;
+  let direction = -1;
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    requestAnimationFrame(animation);
+
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        start: 0,
+        end: window.innerHeight,
+        scrub: true,
+        onUpdate: (e) => (direction = e.direction * -1),
+      },
+      x: "-=300px"
+    });
+  }, []);
+
+  const animation = () => {
+    if (xPercent <= -100) {
+      xPercent = 0;
+    }
+    if (xPercent > 0) {
+      xPercent = -100;
+    }
+    gsap.set(firstText.current, { xPercent: xPercent });
+    gsap.set(secondText.current, { xPercent: xPercent });
+    xPercent += 0.1 * direction;
+    requestAnimationFrame(animation);
+  };
+
+  return (
+    <main className="relative flex h-screen overflow-hidden">
+      <div className="absolute ">
+        <div ref={slider} className="relative whitespace-nowrap">
+          <p ref={firstText} className="relative m-0 text-black text-[9rem] pr-3">
+            Zona Vip BarberShop Estilo y Cuidado Para Ti -
+          </p>
+          <p
+            ref={secondText}
+            className="absolute m-0 text-black text-[9rem] pr-3 left-full top-0"
+          >
+            Zona Vip BarberShop Estilo y Cuidado Para Ti -
+          </p>
+        </div>
+      </div>
+      <div className="h-[400vh]"></div>
+    </main>
+  );
+}
